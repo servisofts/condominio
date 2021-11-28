0aaf00ca9b41ef0d7ba710e49355d31d999dbc52
Roy Ruddy <ruddypazd@gmail.com>
2021-11-28 14:34:53 -0400

    cambios

--- server/src/ManejadorCliente.java
+++ server/src/ManejadorCliente.java
@@ -1,14 +1,15 @@
 import org.json.JSONObject;
 
-import Server.SSSAbstract.SSServerAbstract;
-import Server.SSSAbstract.SSSessionAbstract;
-import Servisofts.SConsole;
-
 public class ManejadorCliente {
     public static void onMessage(JSONObject data, JSONObject config) {
         if (data.isNull("component")) {
             data.put("error", "No existe el componente");
             return;
+        }
+        if(data.has("estado")){
+            if(data.getString("estado").equals("error")){
+                System.out.println(data.getString("error"));
+            }
         }
     }
 
